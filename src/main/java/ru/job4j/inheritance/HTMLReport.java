cf975e0861cd23e4e5c4cbf04e0ad46237729f7c
BashW <BashWhiteShaman@yandex.com>
2022-06-07 10:35:43 +0700

    6. Аннотация @Override [#174891]

--- src/main/java/ru/job4j/inheritance/HTMLReport.java
+++ src/main/java/ru/job4j/inheritance/HTMLReport.java
@@ -1,6 +1,7 @@
 package ru.job4j.inheritance;
 
 public class HTMLReport extends TextReport {
+    @Override
     public String generate(String name, String body) {
         return String.format("<h1>%s</h1><br/><span>%s</span>",
                 name, body);
