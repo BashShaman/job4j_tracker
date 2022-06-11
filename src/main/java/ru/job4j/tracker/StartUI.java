8a4f6ad4c25419d9e6295f30e290443e6684fca0
BashW <BashWhiteShaman@yandex.com>
2022-06-11 11:49:03 +0700

    4.1. Разрыв зависимости StartUI от Scanner. [#181778]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,72 +4,96 @@ import java.util.Scanner;
 
 public class StartUI {
 
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("=== Adding an item ===");
+        Item item = new Item(input.askStr("Enter a name: "));
+        tracker.add(item);
+        System.out.printf("The added item: %s.%s", item,
+                System.lineSeparator());
+    }
+
+    public static void listItems(Input input, Tracker tracker) {
+        System.out.println("=== Listing all items ===");
+        Item[] items = tracker.findAll();
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("The storage has no items yet.");
+        }
+    }
+
+    public static void editItem(Input input, Tracker tracker) {
+        System.out.println("=== Editing an item ===");
+        int id = input.askInt("Enter the ID to find: ");
+        String name = input.askStr("Enter the name to set: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("The operation is successful.");
+        } else {
+            System.out.print("The operation failed. ");
+            System.out.printf("There's NO item with the ID=%d.%s", id,
+                    System.lineSeparator());
+        }
+    }
+
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("=== Deleting an item ===");
+        int id = input.askInt("Enter the ID of the item: ");
+        if (tracker.delete(id)) {
+            System.out.println("The operation is successful.");
+        } else {
+            System.out.print("The operation failed. ");
+            System.out.printf("There's NO item with the ID=%d.%s", id,
+                    System.lineSeparator());
+        }
+    }
+
+    public static void findItemById(Input input, Tracker tracker) {
+        System.out.println("=== Finding an item by ID ===");
+        int id = input.askInt("Enter the ID of the item: ");
+        Item item = tracker.findById(id);
+        if (item != null) {
+            System.out.println(item);
+        } else {
+            System.out.print("The operation failed. ");
+            System.out.printf("There's NO item with the ID=%d.%s", id,
+                    System.lineSeparator());
+        }
+    }
+
+    public static void listItemsByName(Input input, Tracker tracker) {
+        System.out.println("=== Listing items by name ===");
+        String name = input.askStr("Enter the name of the items: ");
+        Item[] items = tracker.findByName(name);
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.printf("There are no items with the name=%s.%s",
+                    name, System.lineSeparator());
+        }
+    }
+
     public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             showMenu();
             int select = input.askInt("Select: ");
             if (select == 0) {
-                System.out.println("=== Adding an item ===");
-                Item item = new Item(input.askStr("Enter a name: "));
-                tracker.add(item);
-                System.out.printf("The added item: %s.%s", item,
-                        System.lineSeparator());
+                createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("=== Listing all items ===");
-                Item[] items = tracker.findAll();
-                if (items.length > 0) {
-                    for (Item item : items) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.println("The storage has no items yet.");
-                }
+                listItems(input, tracker);
             } else if (select == 2) {
-                System.out.println("=== Editing an item ===");
-                int id = input.askInt("Enter the ID to find: ");
-                String name = input.askStr("Enter the name to set: ");
-                Item item = new Item(name);
-                if (tracker.replace(id, item)) {
-                    System.out.println("The operation is successful.");
-                } else {
-                    System.out.print("The operation failed. ");
-                    System.out.printf("There's NO item with the ID=%d.%s", id,
-                            System.lineSeparator());
-                }
+                editItem(input, tracker);
             } else if (select == 3) {
-                System.out.println("=== Deleting an item ===");
-                int id = input.askInt("Enter the ID of the item: ");
-                if (tracker.delete(id)) {
-                    System.out.println("The operation is successful.");
-                } else {
-                    System.out.print("The operation failed. ");
-                    System.out.printf("There's NO item with the ID=%d.%s", id,
-                            System.lineSeparator());
-                }
+                deleteItem(input, tracker);
             } else if (select == 4) {
-                System.out.println("=== Finding an item by ID ===");
-                int id = input.askInt("Enter the ID of the item: ");
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    System.out.println(item);
-                } else {
-                    System.out.print("The operation failed. ");
-                    System.out.printf("There's NO item with the ID=%d.%s", id,
-                            System.lineSeparator());
-                }
+                findItemById(input, tracker);
             } else if (select == 5) {
-                System.out.println("=== Listing items by name ===");
-                String name = input.askStr("Enter the name of the items: ");
-                Item[] items = tracker.findByName(name);
-                if (items.length > 0) {
-                    for (Item item : items) {
-                        System.out.println(item);
-                    }
-                } else {
-                    System.out.printf("There are no items with the name=%s.%s",
-                           name, System.lineSeparator());
-                }
+                listItemsByName(input, tracker);
             } else if (select == 6) {
                 run = false;
             }
