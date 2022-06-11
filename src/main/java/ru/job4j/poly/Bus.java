8e51076f806ada0c62bfd76735bc51fcce2cf998
BashW <BashWhiteShaman@yandex.com>
2022-06-11 09:31:20 +0700

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passengers(int number) {
+    }
+
+    @Override
+    public float fillUp(float amountOfFuel) {
+        return 0;
+    }
+}
